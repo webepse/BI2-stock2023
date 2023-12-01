0b56e5e4016572e4ac44be572fd34b1c8aac8008
berti <berti@CLASSES.LOCAL>
2023-12-01 09:46:36 +0100

    dashboard security

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,11 @@
 <?php 
+    session_start();
+    if(!isset($_SESSION['login']))
+    {
+        header("LOCATION:../403.php");
+    }
+
     require "../connexion.php";
-
-
 ?>
 
 
@@ -16,10 +20,30 @@
     <title>Document</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Stock - Administration</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container-fluid">
      
        
     </div>
-
 </body>
 </html>
